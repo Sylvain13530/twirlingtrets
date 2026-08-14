e6d7c6e3a7e4d4959201c78e31f8c98601ba607f
Sylvain13530 <91609104+Sylvain13530@users.noreply.github.com>
2026-08-14 18:39:51 +0200

    Add files via upload

--- assets/inject-footer.js
+++ assets/inject-footer.js
@@ -1,4 +1,9 @@
 (function () {
+  function setFooterYear(mount) {
+    var year = mount.querySelector('#year');
+    if (year) year.textContent = new Date().getFullYear();
+  }
+
   function injectFooter() {
     var mount = document.getElementById('site-footer');
     if (!mount) return;
@@ -10,12 +15,17 @@
       })
       .then(function (html) {
         mount.innerHTML = html;
+        setFooterYear(mount);
       })
       .catch(function () {
         fetch('footer.html', { cache: 'no-cache' })
-          .then(function (r) { return r.text(); })
+          .then(function (r) {
+            if (!r.ok) throw new Error('no-root-footer');
+            return r.text();
+          })
           .then(function (html) {
             mount.innerHTML = html;
+            setFooterYear(mount);
           })
           .catch(function () { /* noop */ });
       });
